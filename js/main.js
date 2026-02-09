c4d93c80891e14f9fae5698cf18498ca4acb1ac6
Sai Kiran <124028496+saikiran-19906@users.noreply.github.com>
2026-02-09 13:58:24 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,13 @@ $(document).ready(function () {
           items: [
             { type: "video",
           src: "https://player.vimeo.com/video/864138397?h=31c5068d43",ratio: "r-16-9"},
+			   
+		  ]
+        },
+
+		  {
+          type: "grid-mixed",
+          items: [
 			   { type: "video",
           src: "https://player.vimeo.com/video/1163061662?h=31e7f5d1c1",ratio: "r-16-9"},
 		
